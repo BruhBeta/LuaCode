2f229275fb6e5f675fd3ae986934093058906f5d
Gabriel Ordozgoite <Gabrielordozgoite@gmail.com>
2024-01-26 11:05:44 -0400

    Aula20

---
+++ Modulo2/Aula20.lua
@@ -0,0 +1,16 @@
+-- Estrutura de repetição
+-- For
+
+for num = 0,10,1 do
+    print("Ola mundo")
+end
+
+--[[
+for junta todas as caracteristicas do repeat e while juntas,
+é possivel perceber que nem precisei criar uma variavel, pois ela pode ser criada diretamente
+do for.
+Os números que "num" recebe são respectivamente: valor inicial da variavel, quando o loop irá
+parar em relação ao valor de "num" que deve estar, e por final o incremento a cada vez q o loop
+passa pela linha for.
+Se não for informado o valor final, ele será 1.
+]]--
